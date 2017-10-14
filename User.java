72fa43fe0ee59bb1c1bb5cb0e7d46567fb37d230
Agalya7 <31789778+Agalya7@users.noreply.github.com>
2017-10-14 12:18:34 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class User {
     public static void main(String[] args) {
@@ -7,9 +8,23 @@ public class User {
             Socket s = new Socket("localhost", 6666);
             DataInputStream dis = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            Scanner str = new Scanner(System.in);
             String qs = (String)dis.readUTF();
             System.out.print(qs);
-            dout.writeUTF("Agalya");
+            
+            String option = str.next();
+            dout.writeUTF(option);
+            switch(option) {
+                case "1": {
+                    
+                }
+                case "2": {
+                    String stat1 = (String)dis.readUTF();
+                    System.out.print(stat1);
+                    dout.writeUTF(str.next());
+                    //dout.writeUTF()
+                }
+            }
             dout.flush();
             s.close();
         } catch(Exception e) {
